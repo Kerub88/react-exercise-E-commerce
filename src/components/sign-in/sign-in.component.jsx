82626e751c7442f63ac31027cb785c32e1fc7ee4
Simon <kacsa1984@gmail.com>
2020-04-18 20:26:59 +0200

    feat (sign-up): working sign-up and save user to DB 📂 🚀

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -40,7 +40,7 @@ class SignIn extends React.Component {
                         value={this.state.email} 
                         handleChange={this.handleChange}
                         required
-                        label='email'
+                        label='Email'
                     />
 
                     <FormInput 
@@ -49,7 +49,7 @@ class SignIn extends React.Component {
                         value={this.state.password} 
                         handleChange={this.handleChange}
                         required
-                        label='password'
+                        label='Password'
                     />
                     <div className="buttons">
                       <CustomButton type='submit'>Sign In</CustomButton>
