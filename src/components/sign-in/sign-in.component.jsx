739743d51c3fd62efa256aed6f0af8e3850d6ca8
Simon <kacsa1984@gmail.com>
2020-04-18 13:21:24 +0200

    feat (custom-button): component for buttons 📍

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import FormInput from '../form-input/form-input.component'
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -49,7 +50,7 @@ class SignIn extends React.Component {
                         label='password'
                     />
 
-                    <input type='submit' value='Submit Form' />
+                    <CustomButton type='submit'>Sign In</CustomButton>
                 </form>
             </div>
 
